1a261f193614e21b8e61d8eb61e693c37fc25df6
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-03 18:06:38 -0800

    final day 1 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'session/create'
+  get 'session/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#root";
+
 end
